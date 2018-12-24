e38673801089e6e70a3586211454f4c0a8010a47
赵浩翔 <hxzhao@iflytek.com>
2018-12-24 15:51:40 +0800

    refactor: pure go

--- pkg/proxy/response.go
+++ pkg/proxy/response.go
@@ -1,21 +1,24 @@
-package main
+package proxy
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
+var errLogger = log.New(os.Stderr, "", log.LstdFlags)
+
 func ReturnServerError(w http.ResponseWriter, err error) {
 	w.WriteHeader(500)
 	msg := fmt.Sprintf("%v", err)
-	fmt.Fprintf(os.Stderr, "goproxy: %s\n", msg)
-	w.Write([]byte(msg))
+	errLogger.Printf("goproxy: %s\n", msg)
+	_, _ = w.Write([]byte(msg))
 }
 
 func ReturnBadRequest(w http.ResponseWriter, err error) {
 	w.WriteHeader(400)
 	msg := fmt.Sprintf("%v", err)
-	fmt.Fprintf(os.Stderr, "goproxy: %s\n", msg)
-	w.Write([]byte(msg))
+	errLogger.Printf("goproxy: %s\n", msg)
+	_, _ = w.Write([]byte(msg))
 }
