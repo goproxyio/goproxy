6b5ee6198e8ab42596632ab221bbb85468f6e1e4
DennisMao <raymond_2008@yahoo.com>
2019-03-01 18:12:56 +0800

    fix version definition

--- main.go
+++ main.go
@@ -3,9 +3,9 @@
 package main
 
 import (
-	"fmt"
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -17,32 +17,35 @@ import (
 	"github.com/goproxyio/goproxy/pkg/proxy"
 )
 
-const(
-	VERSION = 1.0.0
+const (
+	VERSION = "1.0.0"
 )
 
 var (
-listen string
-cacheDir string
-version bool
+	listen   string
+	cacheDir string
+	version  bool
 )
 
 func init() {
 	log.SetOutput(os.Stdout)
-	flag.BoolVar(&version,"v","display the version")
+	flag.BoolVar(&version, "v", false, "display the version")
 	flag.StringVar(&cacheDir, "cache_dir", "", "go modules cache dir")
 	flag.StringVar(&listen, "listen", "0.0.0.0:8081", "service listen address")
 	flag.Parse()
 }
-// TODO 
+
+// TODO
 // 1.Standardize log's formatting
 // 2.Add http timeout setting
-// 3.Fix the locking problem : cache folder was still locked after the process stopped  
+// 3.Fix the locking problem : cache folder was still locked after the process stopped
 func main() {
 	errCh := make(chan error)
 
 	if version {
-		fmt.Printf("goproxy version is %s \n",VERSION)
+		fmt.Printf("goproxy version is %s \n", VERSION)
+		os.Exit(0)
+		return
 	}
 
 	log.Printf("goproxy: %s inited. listen on %s\n", time.Now().Format("2006-01-02 15:04:05"), listen)
